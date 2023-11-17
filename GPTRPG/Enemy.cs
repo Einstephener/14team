235520888952ec7f8b3e43980de319e622d69e77
Einstephener <kihyuk0201@gmail.com>
2023-11-17 20:12:10 +0900

    참호파기 미니게임
    [ADD]

--- GPTRPG/Enemy.cs
+++ GPTRPG/Enemy.cs
@@ -1,7 +1,7 @@
 ﻿public class Enemy
 {
     public string EnemyName { get; } //적 이름
-    public int EnemyAtk { get; } //적 공격력
+    public int EnemyAtk { get; set; } //적 공격력
     public int EnemyHp { get; set; } //적 체력
 
     public Enemy(string _enemyName, int _enemyAtk, int _enemyHp )
