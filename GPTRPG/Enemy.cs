ee63c55f40c0d94627d48a4336edc277e14eb572
Einstephener <kihyuk0201@gmail.com>
2023-11-17 15:54:04 +0900

    enemyclass

--- GPTRPG/Enemy.cs
+++ GPTRPG/Enemy.cs
@@ -1,14 +1,12 @@
 ﻿public class Enemy
 {
     public string EnemyName { get; } //적 이름
-    public string HowHarder { get; } //출현 이벤트
     public int EnemyAtk { get; } //적 공격력
     public int EnemyHp { get; set; } //적 체력
 
-    public Enemy(string _enemyName, string howHarder, int _enemyAtk, int _enemyHp )
+    public Enemy(string _enemyName, int _enemyAtk, int _enemyHp )
     {
         EnemyName = _enemyName;
-        HowHarder = howHarder;
         EnemyAtk = _enemyAtk;
         EnemyHp = _enemyHp;
 
