5e3fff68aec583b0222ec1f6bc2196b73f113e1e
Einstephener <kihyuk0201@gmail.com>
2023-11-17 12:27:35 +0900

    2023/11/17/ 12/26
    캐릭터, 아이템 클래스 수정

--- GPTRPG/Character.cs
+++ GPTRPG/Character.cs
@@ -39,49 +39,9 @@ public class Character
     //인벤토리에 아이템 추가
     public void AddToInventory(Item item)
     {
-        //아이템이 처음 구매될때 isEquipped가 false로 고정
-        item.IsEquipped = false;
+
         //인벤토리에 아이템 추가
         Inventory.Add(item);
     }
-
-    /*
-    //장비 장착
-    public void EquipItem(int input)
-    {
-        //착용하려는 장비가 인벤토리 안에 있는 장비인가
-        if (input > 0 && input <= Inventory.Count)
-        {
-            
-            //배열은 0부터 시작하기 때문에 input-1
-            var item = Inventory[input - 1];
-
-            if (!item.IsEquipped) //착용여부가 false일때
-            {
-                
-                item.IsEquipped = true;//착용여부를 true로 바꿈
-                Console.WriteLine($" [{item.ItemName}]을(를) 장착했습니다.");
-                //착용된 아이템의 스탯을 캐릭터 스탯에 적용
-                
-                Atk += item.ItemAtk;
-                Def += item.ItemDef;
-            }
-            else
-            {
-                item.IsEquipped = false;//착용여부를 false로 바꿈
-                Console.WriteLine($" [{item.ItemName}]을(를) 해제했습니다.");
-                //착용 해제된 아이템의 스탯을 캐릭터 스탯에 적용
-                Atk -= item.ItemAtk;
-                Def -= item.ItemDef;
-            }
-        }
-        else
-        {
-            Console.WriteLine(" 잘못된 인덱스입니다.");
-        }
-        
-        
-    }
-    */
 
 }
